5fa379d9f70a4b15ac71f7db07732f9f2c0c4dc8
KradChen <87714763@qq.com>
2017-10-01 10:38:28 +0800

    sandun commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,46 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        List<String> aryList = new ArrayList<>(Country.names(20));
-        List<String> linList =  new LinkedList<>(Country.names(20));
-        System.out.printf("ArrayList Printing...");
-        System.out.printf(aryList.toString());
-        System.out.println();
-        Collections.sort(aryList,Collections.reverseOrder());
-        System.out.printf("Sorted ArrayList Printing...");
-        System.out.printf(aryList.toString());
-        System.out.println();
-        System.out.printf("LinkList Printing...");
-        System.out.printf(linList.toString());
-        System.out.println();
-        Collections.shuffle(linList);
-        System.out.printf("Shuffle LinkList Printing...");
-        System.out.printf(linList.toString());
-        System.out.println();
-        Map<String,String> conMap = Country.map;
-        System.out.printf("Map Printing...");
-        System.out.printf(conMap.toString());
-        System.out.println();
-        Map<String,String> newMap = new HashMap<>();
-        for(String name: conMap.keySet())
-        {
-            if(name.startsWith("A")){
-                newMap.put(name,conMap.get(name));
-            }
-        }
-        System.out.printf("Selected Map Printing...");
-        System.out.printf(newMap.toString());
-        System.out.println();
+        SimpleCollectionTest.HashTest();
 
-
-        List<String> txtList = new LinkedList<>();
-        TextFileGenerator tfg = new TextFileGenerator();
-        while (tfg.hasNext()) {
-            txtList.add(tfg.next());
-        }
-        System.out.printf("Text file content Printing...");
-        System.out.printf(txtList.toString());
-        System.out.println();
 
     }
 
