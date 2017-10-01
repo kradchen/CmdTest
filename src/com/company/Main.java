0ab5859de85168dbf08875977ebaeef27ae655a2
KradChen <87714763@qq.com>
2017-10-01 23:09:01 +0800

    sandun commit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,9 @@
 package com.company;
-import java.util.*;
-import java.util.concurrent.locks.ReentrantLock;
+
 
 
 public class Main {
 
-    public static int  count=0;
     public static void main(String[] args) {
 	// write your code here
         ThreadTest t=new ThreadTest();
